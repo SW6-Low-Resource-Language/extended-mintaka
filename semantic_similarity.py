90ddbd1218d97d8787bbe1d6bcd69bc0f84875c3
Lasse Drongesen <Lassemgp@gmail.com>
2025-03-31 11:05:14 +0200

    sem_sim

--- semantic_similarity.py
+++ semantic_similarity.py
@@ -1,4 +1,5 @@
 from sentence_transformers import SentenceTransformer, CrossEncoder
+import json
 
 # Load https://huggingface.co/sentence-transformers/all-mpnet-base-v2
 
@@ -32,7 +33,43 @@ def natural_language_inference(model_answer, dataset_answer):
 
 # Example usage
 if __name__ == "__main__":
-    model_answer = "Ja det er rigtigt"
-    dataset_answer = "Nej det er ikke rigtigt"
-    similarity = semantic_similarity(model_answer, dataset_answer)
-    print(similarity)
+    lang = 'da'
+    with open("./data/mintaka_test_extended.json" , 'r', encoding='utf-8') as file:
+        dataset = json.load(file)
+
+    supporting_ent_map = {}
+    for entry in dataset:
+        id = entry["id"]
+        answer = entry["answer"]
+        supporting_ent_map[id] = None
+        if "supportingEnt" in answer:
+            supporting_ent_map[id] = answer["supportingEnt"]
+    
+    with open("./output_dk_sprgsml_w_id_and_true_answer.json", 'r', encoding='utf-8') as file:
+        answers = json.load(file)
+    
+    for answer in answers:
+        question, answers, id, true_answer = answer['question'], answer['answers'], answer['id'], answer['true_answer']
+        supporting_ent = supporting_ent_map[id]
+        compare_string = str(true_answer)
+        if supporting_ent != None:
+            for entity in supporting_ent:
+                compare_string += ", " + str(entity['label'][lang])
+        best_ans = [0, None]
+        for index, ans in enumerate(answers):
+            sem_score = semantic_similarity(ans, compare_string)
+            if sem_score > best_ans[0]:
+                best_ans[0] = sem_score
+                best_ans[1] = ans
+            print(f"Semantic similarity: {sem_score} - {question} - {ans} - {compare_string}")
+        answer["best_answer"] = best_ans
+    with open("./sem_test.json", 'w', encoding='utf-8') as file:
+        json.dump(answers, file, ensure_ascii=False, indent=4)
+            
+
+
+
+    """    model_answer = "Brad Pitt er højere end george clooney med 8 centimeter"
+        dataset_answer = "Brad pitt"
+        similarity = semantic_similarity(model_answer, dataset_answer)
+        print(similarity) """
